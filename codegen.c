a50f56cab11ed36da50e4c141a84fd92ad83e401
cutsea110 <cutsea110@gmail.com>
2019-05-31 18:42:51 +0900

    codegen care ND_DEREF

--- codegen.c
+++ codegen.c
@@ -18,6 +18,8 @@ void gen_lval(Node* node) {
     printf("  mov rax, rbp\n");
     printf("  sub rax, %d\n", (offset + 1) * 8);
     printf("  push rax\n");
+  } else if (node->ty == ND_DEREF) {
+    gen(node->lhs);
   } else
     error("代入の左辺値が変数ではありません");
 }
@@ -26,6 +28,21 @@ void gen(Node* node) {
 
   if (node->ty == ND_VARDEF) {
     DEBUG("ND_VARDEF Found");
+    return;
+  }
+
+  if (node->ty == ND_DEREF) {
+    DEBUG("ND_DEREF Found");
+    gen(node->lhs);
+    printf("  pop rax\n");
+    printf("  mov rax, [rax]\n");
+    printf("  push rax\n");
+    return;
+  }
+
+  if (node->ty == ND_REF) {
+    DEBUG("ND_REF Found");
+    // TODO
     return;
   }
   
